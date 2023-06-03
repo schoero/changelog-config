abc5aa1ba2e78329cbe16c26ecdda7f8baf42b90
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2023-06-03 07:35:26 +0200

    feat: initial version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # changelog-config
+
 Shared config for changelogen
+
+## Installation
+
+```sh
+npm i --save-dev @schoero/eslint-config
+```
+
+## Usage
+
+Create a `changelog.config.js` with the following content:
+
+```js
+export * from "@schoero/changelog-config";
+```
---
+++ changelog.config.js
@@ -0,0 +1,24 @@
+/** @type {import('changelogen').ChangelogConfig} */
+export default {
+  output: "CHANGELOG.md",
+  scopeMap: {},
+  templates: {
+    commitMessage: "chore(release): v{{newVersion}}",
+    tagBody: "v{{newVersion}}",
+    tagMessage: "v{{newVersion}}"
+  },
+  types: {
+    build: { semver: "patch", title: "Build" },
+    chore: { title: "Chore" },
+    ci: { title: "CI" },
+    docs: { semver: "patch", title: "Documentation" },
+    examples: { title: "Examples" },
+    feat: { semver: "minor", title: "Features" },
+    fix: { semver: "patch", title: "Fixes" },
+    perf: { semver: "patch", title: "Performance" },
+    refactor: { semver: "patch", title: "Refactors" },
+    style: { title: "Styles" },
+    test: { title: "Tests" },
+    types: { semver: "patch", title: "Types" }
+  }
+};
